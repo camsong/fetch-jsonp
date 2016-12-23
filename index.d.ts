43d7560fdae51e848029b74af8b5ad7fe24200f1
Cam Song <neosoyn@gmail.com>
2016-12-23 18:09:50 +0800

    upadte typescript d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
-declare function fetchJsonp(url:string):Function;
+export interface FetchJsonpOptions {
+  timeout?: number;
+  jsonpCallback?: string;
+  callbackFunction?: string;
+}
+
+declare function fetchJsonp(url:string, options?:FetchJsonpOptions):Function;
 
 export default fetchJsonp;
